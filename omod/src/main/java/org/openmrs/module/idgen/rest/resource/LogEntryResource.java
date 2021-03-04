49e3cd97723e68b88922772ca4a15e7840afe4a1
dkayiwa <kayiwadaniel@gmail.com>
2021-03-04 15:06:44 +0300

    Add support for platform 2.5

--- omod/src/main/java/org/openmrs/module/idgen/rest/resource/LogEntryResource.java
+++ omod/src/main/java/org/openmrs/module/idgen/rest/resource/LogEntryResource.java
@@ -44,7 +44,7 @@ import io.swagger.models.properties.StringProperty;
 
 @Resource(name = RestConstants.VERSION_1 + IdgenRestController.IDGEN_NAMESPACE
 		+ "/logentry", supportedClass = LogEntry.class, supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*",
-				"1.12.*", "2.0.*", "2.1.*", "2.2.*", "2.3.*", "2.4.*" })
+				"1.12.*", "2.0.*", "2.1.*", "2.2.*", "2.3.*", "2.4.*", "2.5.*" })
 public class LogEntryResource extends MetadataDelegatingCrudResource<LogEntry> {
 
 	@Override
