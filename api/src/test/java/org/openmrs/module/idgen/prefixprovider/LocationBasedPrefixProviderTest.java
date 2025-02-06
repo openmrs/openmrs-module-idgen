460ee49ad373d1a999dd60a0ca27cc3c4ab36131
Wikum Weerakutti <wikum@linuxdeveloper.space>
2025-02-06 12:55:08 +0530

    Update the platform version to 2.0.0

--- api/src/test/java/org/openmrs/module/idgen/prefixprovider/LocationBasedPrefixProviderTest.java
+++ api/src/test/java/org/openmrs/module/idgen/prefixprovider/LocationBasedPrefixProviderTest.java
@@ -11,7 +11,6 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.MockedStatic;
 import org.openmrs.Location;
 import org.openmrs.LocationAttribute;
